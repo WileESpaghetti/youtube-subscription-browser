a58ea57bcece7b1931281253e4e62df791a3e567
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-03-23 14:50:14 -0400

    ignore duplicate channel inserts

--- schema/channels.sql
+++ schema/channels.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS channels (
     id INTEGER PRIMARY KEY,
-    youtube_id TEXT NOT NULL UNIQUE,
+    youtube_id TEXT NOT NULL,
     title TEXT,
     description TEXT,
     custom_url TEXT,
     branding_title TEXT,
     branding_description TEXT,
     subscriber_count INTEGER NOT NULL default 0,
-    video_count INTEGER NOT NULL default 0
+    video_count INTEGER NOT NULL default 0,
+    UNIQUE(youtube_id) ON CONFLICT IGNORE
 );
