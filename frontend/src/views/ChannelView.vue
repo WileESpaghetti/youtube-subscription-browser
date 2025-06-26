885769f42eb39741f8157a12b68e3c1059aec910
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-06-25 21:21:58 -0400

    show video stats on the frontend

--- frontend/src/views/ChannelView.vue
+++ frontend/src/views/ChannelView.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="about">
-    <template v-if="isFetching">
+    <template v-if="isFetching || isFetchingStats">
       <h1 >Loading data...</h1>
     </template>
     <template v-else>
       <h1>{{data?.title}}</h1>
       <p>{{data?.description}}</p>
+      <ul v-if="statData">
+        <li>Total Videos: {{data?.video_count}}</li>
+        <li>{{statData?.has_archive ? `Archived: ${statData?.total_videos_archived}` : 'Not Archived' }}</li>
+        <li>Last Upload Date: {{lastUploadDate}}</li>
+      </ul>
       <VideoChart :channelId="Number(route.params.id)"></VideoChart>
     </template>
   </div>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 import {useFetch} from "@vueuse/core";
 import {useRoute} from 'vue-router'
 import VideoChart from "@/components/VideoChart.vue";
 
 const route = useRoute();
 const { data, error, isFetching, execute, abort } = useFetch(`/api/channels/${route.params.id}`).json();
+const { data: statData, isFetching: isFetchingStats } = useFetch(`/api/channels/${route.params.id}/video_stats`).json();
+const lastUploadDate = computed(() => {
+  if (!statData?.value?.latest_video_upload_date) {
+    return null;
+  }
+
+  return new Date(statData.value.latest_video_upload_date * 1000).toLocaleDateString()
+});
 </script>
 
 <style>
