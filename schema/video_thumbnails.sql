60f1af6ad0fccd57551dff0c7023fd031680af89
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-09-01 20:24:50 -0400

    add video thumbnails

---
+++ schema/video_thumbnails.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS video_thumbnails (
+    id INTEGER PRIMARY KEY,
+    video_id INTEGER NOT NULL,
+    size TEXT NOT NULL,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    url TEXT NOT NULL,
+    FOREIGN KEY(video_id) REFERENCES videos(id) ON DELETE CASCADE
+)
