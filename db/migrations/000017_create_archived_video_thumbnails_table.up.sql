a4fdcd6cb56427b5f614d984734f8e03963dd351
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-09-20 15:17:54 -0400

    add archived video thumbnails table It felt kind of messy to try and merge this data with the video_thumbnails table. This table probably works better as a fallback instead of a main place to pull video images.

---
+++ db/migrations/000017_create_archived_video_thumbnails_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS archived_video_thumbnails (
+    id INTEGER PRIMARY KEY,
+    video_id INTEGER NOT NULL,
+    resolution TEXT,
+    index_id TEXT,
+    preference INTEGER,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    url TEXT NOT NULL,
+    file_name TEXT,
+    FOREIGN KEY(video_id) REFERENCES videos(id) ON DELETE CASCADE
+);
