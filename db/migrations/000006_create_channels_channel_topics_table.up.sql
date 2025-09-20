54c0ad241eb9a606203a29d9c6f678f01d8c5382
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-09-20 19:20:18 -0400

    fix bad foreign key

--- db/migrations/000006_create_channels_channel_topics_table.up.sql
+++ db/migrations/000006_create_channels_channel_topics_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS channels_channel_topics (
     channel_id INTEGER,
     topic_id INTEGER,
     FOREIGN KEY(channel_id) REFERENCES channels(id) ON DELETE CASCADE,
-    FOREIGN KEY(topic_id) REFERENCES topics(id) ON DELETE CASCADE,
+    FOREIGN KEY(topic_id) REFERENCES channel_topics(id) ON DELETE CASCADE,
     UNIQUE (channel_id, topic_id) ON CONFLICT IGNORE
 );
