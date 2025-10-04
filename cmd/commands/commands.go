ce2e86025d31059b89e9e5e12d947b17bbd01eba
Lehman Black <WileESpaghetti@users.noreply.github.com>
2025-10-04 17:21:44 -0400

    add some cache defaults for commands

--- cmd/commands/commands.go
+++ cmd/commands/commands.go
@@ -3,15 +3,19 @@ package commands
 import youtube_subscription_browser "github.com/WileESpaghetti/youtube-subscription-browser"
 
 type Context struct {
-	Verbose   bool   `help:"Enable verbose mode."`
-	Database  string `help:"Database file." default:"./youtube.sqlite"`
-	TokenFile string `help:"OAuth authorization token." default:""`
+	Verbose      bool   `help:"Enable verbose mode."`
+	Database     string `help:"Database file." default:"./youtube.sqlite"`
+	TokenFile    string `help:"OAuth authorization token." default:""`
+	CacheDir     string `help:"Cache directory." default:"./cache"`
+	DisableCache bool   `help:"Disable cache."`
 }
 
 func NewContext() *Context {
 	return &Context{
-		Verbose:   false,
-		Database:  "./youtube.sqlite",
-		TokenFile: youtube_subscription_browser.DefaultTokenFile,
+		Verbose:      false,
+		Database:     "./youtube.sqlite",
+		TokenFile:    youtube_subscription_browser.DefaultTokenFile,
+		CacheDir:     "./cache",
+		DisableCache: false,
 	}
 }
